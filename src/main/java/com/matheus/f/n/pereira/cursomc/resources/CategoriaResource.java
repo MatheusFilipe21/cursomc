b959945a1d1ced7e94292e4324b700360a62fdbf
MatheusFilipe21 <matheusfilipe1999@gmail.com>
2021-02-21 21:18:46 -0300

    Configuracoes iniciais e criacao da entidade Categoria

---
+++ src/main/java/com/matheus/f/n/pereira/cursomc/resources/CategoriaResource.java
@@ -0,0 +1,11 @@
+package com.matheus.f.n.pereira.cursomc.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoriaResource {
+	
+	
+}

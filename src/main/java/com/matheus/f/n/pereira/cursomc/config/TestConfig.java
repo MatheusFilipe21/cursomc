7455b60672c97c6cea117d4b7765241d0bef864e
MatheusFilipe21 <matheusfilipe1999@gmail.com>
2021-02-22 07:43:21 -0300

    Criando as entidades Estado e Cidade

--- src/main/java/com/matheus/f/n/pereira/cursomc/config/TestConfig.java
+++ src/main/java/com/matheus/f/n/pereira/cursomc/config/TestConfig.java
@@ -8,8 +8,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.matheus.f.n.pereira.cursomc.entities.Categoria;
+import com.matheus.f.n.pereira.cursomc.entities.Cidade;
+import com.matheus.f.n.pereira.cursomc.entities.Estado;
 import com.matheus.f.n.pereira.cursomc.entities.Produto;
 import com.matheus.f.n.pereira.cursomc.repositories.CategoriaRepository;
+import com.matheus.f.n.pereira.cursomc.repositories.CidadeRepository;
+import com.matheus.f.n.pereira.cursomc.repositories.EstadoRepository;
 import com.matheus.f.n.pereira.cursomc.repositories.ProdutoRepository;
 
 @Configuration
@@ -21,6 +25,12 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
 	
 	@Override
 	public void run(String... args) throws Exception {
@@ -41,6 +51,17 @@ public class TestConfig implements CommandLineRunner {
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 		
 	}
 

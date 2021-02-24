bcb79ee8126c0419e6789f9e0d27fddf0a5c4d1a
MatheusFilipe21 <matheusfilipe1999@gmail.com>
2021-02-23 21:13:11 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/com/matheus/f/n/pereira/cursomc/config/TestConfig.java
+++ src/main/java/com/matheus/f/n/pereira/cursomc/config/TestConfig.java
@@ -13,6 +13,7 @@ import com.matheus.f.n.pereira.cursomc.entities.Cidade;
 import com.matheus.f.n.pereira.cursomc.entities.Cliente;
 import com.matheus.f.n.pereira.cursomc.entities.Endereco;
 import com.matheus.f.n.pereira.cursomc.entities.Estado;
+import com.matheus.f.n.pereira.cursomc.entities.ItemPedido;
 import com.matheus.f.n.pereira.cursomc.entities.Pagamento;
 import com.matheus.f.n.pereira.cursomc.entities.PagamentoComBoleto;
 import com.matheus.f.n.pereira.cursomc.entities.PagamentoComCartao;
@@ -25,6 +26,7 @@ import com.matheus.f.n.pereira.cursomc.repositories.CidadeRepository;
 import com.matheus.f.n.pereira.cursomc.repositories.ClienteRepository;
 import com.matheus.f.n.pereira.cursomc.repositories.EnderecoRepository;
 import com.matheus.f.n.pereira.cursomc.repositories.EstadoRepository;
+import com.matheus.f.n.pereira.cursomc.repositories.ItemPedidoRepository;
 import com.matheus.f.n.pereira.cursomc.repositories.PagamentoRepository;
 import com.matheus.f.n.pereira.cursomc.repositories.PedidoRepository;
 import com.matheus.f.n.pereira.cursomc.repositories.ProdutoRepository;
@@ -56,6 +58,9 @@ public class TestConfig implements CommandLineRunner {
 
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -115,6 +120,19 @@ public class TestConfig implements CommandLineRunner {
 		
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+		
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
 	}
 
 }

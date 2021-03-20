7fc4abcc756a042cd8024471fd428086762fc170
MatheusFilipe21 <matheusfilipe1999@gmail.com>
2021-03-20 18:18:44 -0300

    Restricao de conteudo: cliente so recupera seus pedidos

--- src/main/java/com/matheus/f/n/pereira/cursomc/services/PedidoService.java
+++ src/main/java/com/matheus/f/n/pereira/cursomc/services/PedidoService.java
@@ -1,13 +1,16 @@
 package com.matheus.f.n.pereira.cursomc.services;
 
 import java.util.Date;
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.matheus.f.n.pereira.cursomc.entities.Cliente;
 import com.matheus.f.n.pereira.cursomc.entities.ItemPedido;
 import com.matheus.f.n.pereira.cursomc.entities.PagamentoComBoleto;
 import com.matheus.f.n.pereira.cursomc.entities.Pedido;
@@ -15,6 +18,8 @@ import com.matheus.f.n.pereira.cursomc.entities.enums.EstadoPagamento;
 import com.matheus.f.n.pereira.cursomc.repositories.ItemPedidoRepository;
 import com.matheus.f.n.pereira.cursomc.repositories.PagamentoRepository;
 import com.matheus.f.n.pereira.cursomc.repositories.PedidoRepository;
+import com.matheus.f.n.pereira.cursomc.security.UserSpringSecurity;
+import com.matheus.f.n.pereira.cursomc.services.exceptions.AuthorizationException;
 import com.matheus.f.n.pereira.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -34,16 +39,12 @@ public class PedidoService {
 
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
-	
+
 	@Autowired
 	private ClienteService clienteService;
-	
+
 	@Autowired
 	private EmailService emailService;
-
-	public List<Pedido> findAll() {
-		return repository.findAll();
-	}
 
 	public Pedido findById(Integer id) {
 		Optional<Pedido> obj = repository.findById(id);
@@ -71,5 +72,15 @@ public class PedidoService {
 		itemPedidoRepository.saveAll(obj.getItens());
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
+	}
+
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSpringSecurity user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteService.findById(user.getId());
+		return repository.findByCliente(cliente, pageRequest);
 	}
 }
